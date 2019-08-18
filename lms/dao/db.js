f93acaf64903667260f5055b3b3335366a6eddee
CodesForFood <ecordero8192@live.com>
2019-08-18 10:28:07 -0400

    amazon connection

--- lms/dao/db.js
+++ lms/dao/db.js
@@ -1,9 +1,10 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host     : '127.0.0.1',
+    host     : 'mydatabase.c3m6vgjobhcf.us-east-2.rds.amazonaws.com',
+    port     : '3306',
     user     : 'root',
-    password : 'root',
+    password : 'rootroot',
     database : 'lms'
 });
 
