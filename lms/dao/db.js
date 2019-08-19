1dca35f62da39ee1459043940f7299a85b3ebdf3
Emmanuel <ecordero8192@live.com>
2019-08-19 16:54:13 -0400

    change

--- lms/dao/db.js
+++ lms/dao/db.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host     : 'mydatabase.c3m6vgjobhcf.us-east-2.rds.amazonaws.com',
+    host     : 'emandb.c3m6vgjobhcf.us-east-2.rds.amazonaws.com',
     port     : '3306',
     user     : 'root',
     password : 'rootroot',
